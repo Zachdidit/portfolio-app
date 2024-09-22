3adb8e2fc435d52dca3cc20e628b7b2fb02718c1
Zach White <zachdidit@gmail.com>
2024-09-21 21:42:42 -0700

    Sectioning off header and footer into seperate pages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 
 import { Heading, Text, Flex, Button, Grid, Icon, Background, Avatar, LetterFx } from '@/once-ui/components';
-import Image from 'next/image'
 import Link from 'next/link';
 
 export default function Home() {
@@ -27,18 +26,11 @@ export default function Home() {
 	];
 
 	return (
-		<Flex
-			fillWidth paddingTop="l" paddingX="l"
-			direction="column" alignItems="center" flex={1}>
-			<Background
-				dots={false}
-				gradient={true}
-				lines={false}/>
 			<Flex
 				position="relative"
 				as="section" overflow="hidden"
-				fillWidth minHeight="0" maxWidth={85}
-				direction="column" alignItems="center" flex={1}>
+				fillWidth minHeight="0"
+				direction="row" alignItems="center" flex={1}>
 				<Flex
 					as="main"
 					direction="column" justifyContent="center"
@@ -73,65 +65,8 @@ export default function Home() {
 							</Text>
 						</Flex>
 					</Flex>
-					<Grid
-						radius="l"
-						border="neutral-medium"
-						borderStyle="solid-1"
-						columns="repeat(3, 1fr)"
-						tabletColumns="1col"
-						mobileColumns="1col"
-						fillWidth>
-						{links.map((link) => (
-							<Link
-								target="_blank"
-								style={{ padding: 'var(--responsive-space-l)' }}
-								key={link.href}
-								href={link.href}>
-								<Flex
-									fillWidth paddingY="8" gap="8"
-									direction="column">
-									<Flex
-										fillWidth gap="12"
-										alignItems="center">
-										<Text
-											variant="body-strong-m" onBackground="neutral-strong">
-											{link.title}
-										</Text>
-										<Icon size="s" name="arrowUpRight" />
-									</Flex>
-									<Text
-										variant="body-default-s" onBackground="neutral-weak">
-										{link.description}
-									</Text>
-								</Flex>
-							</Link>
-						))}
-					</Grid>
 				</Flex>
 			</Flex>
-			<Flex
-				as="footer"
-				position="relative"
-				fillWidth paddingX="l" paddingY="m"
-				justifyContent="space-between">
-				<Text
-					variant="body-default-s" onBackground="neutral-weak">
-					© 2024 Zachary White
-				</Text>
-				<Flex
-					gap="12">
-					<Button
-						href="https://github.com/Zachdidit"
-						prefixIcon="github" size="s" variant="tertiary">
-						GitHub
-					</Button>
-					<Button
-						href="https://www.linkedin.com/in/zach-white-7a96106/"
-						prefixIcon="linkedin" size="s" variant="tertiary">
-						Linkedin
-					</Button>
-				</Flex>
-			</Flex>
-		</Flex>
+
 	);
 }
